f9d58ec5e5c3ba80503f40dc53c7827873aa335a
Coder-Hanye <239064514@qq.com>
2024-07-12 10:46:35 +0800

    Add files via upload

---
+++ test_7_03_2024/test_7_03_2024/test.c
@@ -0,0 +1,69 @@
+
+//#include<Stdio.h>
+//int main()
+//{
+//	int ha = 9;
+//	char no = 'nnn';
+//	long num = 999;
+//	float weight = 66.7;
+//	printf("hello world\n");
+//	printf("500\n");
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(float));
+//
+//		return 0;
+//}
+//第一次练习（内心PS不，还没学好怎么把代码上传到github/gitee)
+
+
+//#include<Stdio.h>
+//int main()
+//{
+//	int age = 18;
+//	double weight = 47.5;
+//	
+//	printf(" % d\n", age);
+//	printf(" % lf\n", weight);
+//
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<Stdio.h>
+//int main()
+//{
+//	int a = 99;
+//	int b = 0;
+//	int sum = 0;
+//	scanf(" %d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum=%d\n", sum);
+//
+//	return 0;
+//}
+//古老的写法，不建议用
+//void main()
+//{
+//
+//
+//}
+#include<Stdio.h>
+int main()
+{
+	int a = 7;
+	{
+		int a = 5;
+		printf("a=% d\n", a);
+	}
+	return 0;
+}
